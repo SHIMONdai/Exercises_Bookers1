71e176be6be03d9b02eeddf986cff6847d19aaae
EC2 Default User <ec2-user@ip-172-31-14-221.ec2.internal>
2022-02-16 03:40:46 +0000

    [add]show.edit.destroy function

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get 'books' => 'books#index', as: 'book'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'show_book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  delete 'books/:id' => 'books#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
